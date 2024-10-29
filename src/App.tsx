1f7b541924d01cbf0c825fce2a09dfcaaa7bbdb1
aivanns <a1vazovski@yandex.ru>
2024-10-29 23:41:23 +0300

    #3 Добавил экран с чатами

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,23 @@ import { Route, Routes } from 'react-router-dom'
 import AuthPage from './pages/auth/auth-page'
 import RegistrationForm from './components/auth/registration-form'
 import AuthForm from './components/auth/auth-form'
+import { Navigate } from 'react-router-dom'
+import ChatsPage from './pages/chats/chats-page'
 
 function App() {
 
   return (
     <>
       <Routes>
-        <Route path="" element={<></>} />
+        <Route path="" element={<Navigate to="auth/register" />} />
 
         <Route path="/auth" element={<AuthPage />}>
+          <Route index element={<Navigate to="register" />} />
           <Route path="login" element={<AuthForm />} />
           <Route path="register" element={<RegistrationForm />} />
         </Route>
+
+        <Route path="/chats" element={<ChatsPage />} />
 
       </Routes>
     </>
---
+++ src/components/chats/chat-sidebar.tsx
@@ -0,0 +1,17 @@
+import { FaCog } from "react-icons/fa";
+import avatar from '../../assets/avatar.svg';
+
+const ChatSidebar = () => {
+    return (
+        <div className='h-[55rem] w-20 bg-main-1 rounded-r-2xl self-center resize-none flex-shrink-0 flex flex-col justify-between'>
+            <div className='flex justify-center'>
+                <img src={avatar} alt="avatar" className='w-14 h-14 rounded-full mt-3' />
+            </div>
+            <div className='flex justify-center mb-5'>
+                <FaCog className="text-4xl text-gray-300 hover:text-main-4 cursor-pointer" />
+            </div>
+        </div>
+    )
+}
+
+export default ChatSidebar;
