757df6d6bf4d2b04fbb19c48d9fc565621bcbbb0
aivanns <a1vazovski@yandex.ru>
2024-11-01 12:08:23 +0300

    #3 Исправил компонент сообщения

--- src/components/chats/chat-message.tsx
+++ src/components/chats/chat-message.tsx
@@ -1,17 +1,17 @@
 import { ChatMessageProps } from '../../shared/interfaces/chats';
 
-const ChatMessage = ({message, avatar, isLastInSequence}: ChatMessageProps) => {
+const ChatMessage = ({ message, avatar, isLastInSequence }: ChatMessageProps) => {
     return (
         <div className="flex items-start mb-4 ml-8">
             <div className="w-10 h-10 rounded-full bg-gray-300 flex-shrink-0 mr-3">
                 {isLastInSequence && (
-                    <img src={avatar} alt="" />
+                    <img src={avatar} alt="Avatar" className="w-full h-full rounded-full object-cover" />
                 )}
             </div>
             <div className="flex flex-col w-full">
-                <div className="flex bg-main-3 rounded-lg py-2 px-3 max-w-2xl ml-0">
-                    <p className="text-sm text-text-color inline">{message}</p>
-                    <span className="text-xs text-text-color ml-2 mt-2 leading-tight">{'12:00'}</span>
+                <div className="bg-main-3 rounded-lg py-2 px-3 max-w-2xl w-full break-words relative">
+                    <span className="text-sm text-text-color pr-12">{message}</span>
+                    <span className="text-xs text-text-color absolute bottom-2 right-3">{'12:00'}</span>
                 </div>
             </div>
         </div>
