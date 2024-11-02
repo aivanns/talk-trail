6fca02120783c9870911f1b753e64e4a7647f54f
aivanns <a1vazovski@yandex.ru>
2024-11-02 13:14:01 +0300

    #6 Переписал логику работы аутентификации

--- src/shared/utils/tokenService.ts
+++ src/shared/utils/tokenService.ts
@@ -1,6 +1,7 @@
 import { jwtDecode } from 'jwt-decode';
 import Cookies from 'js-cookie';
 import type { DecodedToken } from '../interfaces/auth';
+import { apiInstance } from '../../api/global';
 
 export const setToken = (token: string, refreshToken: string | null = null) => {
   Cookies.set('token', token, { secure: true, sameSite: 'Strict' });
@@ -20,21 +21,40 @@ export const removeToken = (refreshToken = false) => {
   }
 };
 
-export const isAuthenticated = (): boolean => {
+export const getRefreshToken = () => {
+  return Cookies.get('refreshToken');
+};
+
+export const isAuthenticated = async (): Promise<boolean> => {
   const token = getToken();
   if (!token) return false;
   
   try {
     const decodedToken: DecodedToken = jwtDecode(token);
     const { exp } = decodedToken;
+    
     if (typeof exp === 'number' && exp < Date.now() / 1000) {
-      removeToken();
-      return false;
+      const refreshToken = getRefreshToken();
+      if (!refreshToken) {
+        removeToken(true);
+        return false;
+      }
+
+      try {
+        const { data } = await apiInstance.post('/auth/refresh', { refreshToken });
+        const { token: newToken, refreshToken: newRefreshToken } = data;
+        setToken(newToken, newRefreshToken);
+        return true;
+      } catch (error) {
+        console.error('Ошибка обновления токена:', error);
+        removeToken(true);
+        return false;
+      }
     }
     return true;
   } catch (e) {
     console.error('Ошибка декодирования токена:', e);
-    removeToken();
+    removeToken(true);
     return false;
   }
 };
