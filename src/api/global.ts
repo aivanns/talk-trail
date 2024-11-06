e2b48042208f7dd26db35870df4bc118ce327d35
aivanns <a1vazovski@yandex.ru>
2024-11-06 11:38:24 +0300

    #FIX: Исправлено поведение при ошибке 401

--- src/api/global.ts
+++ src/api/global.ts
@@ -3,6 +3,7 @@ import { getToken } from '../shared/utils/services/tokenService';
 import notificationService from '../shared/utils/services/notificationService';
 import { ERROR } from '../shared/constants/notification';
 import { SKIP_ERROR_NOTIFICATION_ENDPOINTS } from '../shared/constants/interceptor';
+import { ROUTES } from '../shared/constants/routes';
 
 export const apiInstance = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
@@ -16,6 +17,11 @@ export const getHeaders = () => ({
 apiInstance.interceptors.response.use(
     (response) => response,
     (error) => {
+        if (error.response?.status === 401) {
+            window.location.href = ROUTES.AUTH.LOGIN;
+            return Promise.reject(error);
+        }
+
         const isSkipNotification = SKIP_ERROR_NOTIFICATION_ENDPOINTS.some(endpoint => 
             error.config?.url?.includes(endpoint)
         );
